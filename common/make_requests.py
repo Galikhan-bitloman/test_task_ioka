ba83e495d9805426250adc2868f141e5bac7f049
galikhan <galihan.abdikarim@gmail.com>
2023-10-10 20:12:24 +0600

    checking for literals in requesting func

--- common/make_requests.py
+++ common/make_requests.py
@@ -1,6 +1,3 @@
-from typing import Optional
-
-from requests import Response
 import requests
 
 from common.consts import HTTP_METHOD, HEADERS
@@ -13,10 +10,11 @@ class Requesting:
     def make_requests(self, method: str,path: str, params: dict = None) -> dict:
         try:
             headers = {"API-KEY": HEADERS.API_KEY.value, "Content-Type": "application/json"}
-            if method == HTTP_METHOD.GET.value:
-                req = requests.get(url=self.base_url + path, headers=headers)
+            if method == HTTP_METHOD.GET.value or method == "get":
+
+                req = requests.get(url=self.base_url + path, headers=headers, params=params)
                 return req.json()
-            if method == HTTP_METHOD.POST.value:
+            if method == HTTP_METHOD.POST.value or method == "post":
                 req = requests.post(url=self.base_url + path, json=params, headers=headers)
                 return req.json()
         except (ConnectionError, TimeoutError) as e:
