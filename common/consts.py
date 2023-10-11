fd5e925ccb5e8c4e5b2bcc93bcd413676cfda9a3
galikhan <galihan.abdikarim@gmail.com>
2023-10-11 18:43:26 +0600

    replacing hardcoding to envs

--- common/consts.py
+++ common/consts.py
@@ -1,10 +1,16 @@
 from enum import Enum
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class URL(Enum):
     API_HOST = os.getenv('API_HOST')
     CREATE_ORDER = os.getenv('CREATE_ORDER')
     GET_ORDERS = os.getenv('GET_ORDERS')
+    GET_CUSTOMERS = os.getenv('GET_CUSTOMERS')
+    CREATE_CUSTOMERS = os.getenv('CREATE_CUSTOMERS')
+
 
 
 class HTTP_METHOD(Enum):
@@ -15,4 +21,15 @@ class HTTP_METHOD(Enum):
 class HEADERS(Enum):
     API_KEY = os.getenv('API_KEY')
 
+
+class Status(Enum):
+    PENDING = "PENDING"
+    READY = "READY"
+
+class DateCategory(Enum):
+    DAILY = "DAILY"
+    MONTHLY = "MONTHLY"
+    QUARTERLY = "QUARTERLY"
+    YEARLY = "YEARLY"
+    MANUAL = "MANUAL"
 
