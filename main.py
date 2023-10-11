975f492be4226a7b41800621a57186dcc0cd1791
galikhan <galihan.abdikarim@gmail.com>
2023-10-11 18:46:50 +0600

    freezed dotenv, minor fix in main file

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-
 from framework.order import Order
+from framework.customer import Customer
+
 # make an instance of Order class
-base = Order()
+baseOrder = Order()
+
 # use createOrder method of Order class
-create = base.createOrder(amount=100000, capture_method="MANUAL")
+createOrder = baseOrder.createOrder(amount=100000, capture_method="MANUAL")
 
 # get anything you want from mapped pydantic model  according to response json
-print(create.order.amount)
+print(createOrder.order.amount) #amount = 111111
 
 
 # get all orders
-get = base.getOrders()
-# all orders in list so you can get dict by index
-print(get[1].amount)
+getOrders = baseOrder.getOrders()
+# all the orders in list so you can get dict by index
+print(getOrders[1].amount)
+
+
+baseCustomer = Customer()
+
+getCustomers = baseCustomer.getCustomers(limit=4, page=1)
+
+print("getCustomers", getCustomers[0].email)
+
+createCustomers = baseCustomer.createCustomer(email="a@gmail.com", phone="77777777")
+
+print("createCustomers", createCustomers)
+
+
+
+
 
 
 
