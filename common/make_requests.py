dcaf7868b110d717a59f67ed323b8d219d9f89f1
galikhan <galihan.abdikarim@gmail.com>
2023-10-11 18:45:34 +0600

    added handling 4xx, 5xx errors when making_requests

--- common/make_requests.py
+++ common/make_requests.py
@@ -1,3 +1,4 @@
+from requests import HTTPError
 import requests
 
 from common.consts import HTTP_METHOD, HEADERS
@@ -13,9 +14,13 @@ class Requesting:
             if method == HTTP_METHOD.GET.value or method == "get":
 
                 req = requests.get(url=self.base_url + path, headers=headers, params=params)
+                print("req", req)
                 return req.json()
             if method == HTTP_METHOD.POST.value or method == "post":
                 req = requests.post(url=self.base_url + path, json=params, headers=headers)
                 return req.json()
         except (ConnectionError, TimeoutError) as e:
             return e
+        except HTTPError as httpE:
+            print("httpE", httpE)
+            return httpE
